6f89d4c1524fef35b2cfa51f32f392172400f24c
UllernJ <Ulrik.johannesen@gmail.com>
2022-11-01 15:45:55 +0100

    Added db and seeding.
    Also fixed in tsconfig file.

--- oppgave_2/prisma/seed.ts
+++ oppgave_2/prisma/seed.ts
@@ -3,16 +3,47 @@ import { faker } from '@faker-js/faker'
 
 const prisma = new PrismaClient()
 
+const groups = ["Informasjonssystemer", "Informatikk", "Digitale medier og design"]
+
+const fakeFactory = (count: number) => {
+  return Array(count)
+  .fill(null)
+  .map(() => {
+    return {
+      id: faker.git.commitSha(),
+      name: faker.name.firstName(),
+      gender: faker.name.sexType(),
+      age: faker.datatype.number({
+        'min': 18,
+        'max': 50
+      }),
+      group: groups[Math.floor(Math.random() * groups.length)]
+    }
+  })
+}
+
+
+
 async function main() {
   console.log(`Start seeding ...`)
+  try {
+    const students = fakeFactory(50)
+    for(const student of students) {
+      await prisma.student.create({
+        data: student
+      })
+    }
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  } finally {
+    await prisma.$disconnect()
+  }
+
+
+
   console.log(`Seeding finished.`)
 }
 
 main()
-  .catch((e) => {
-    console.error(e)
-    process.exit(1)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+

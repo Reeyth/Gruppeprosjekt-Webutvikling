dd24e0730d000e29cb1b7c7ad25fad6a7ae3e64f
UllernJ <Ulrik.johannesen@gmail.com>
2022-11-01 15:54:03 +0100

    api now gets all information from db

--- oppgave_2/pages/api/students.ts
+++ oppgave_2/pages/api/students.ts
@@ -1,4 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
 
 type Student = {
   id: string
@@ -12,5 +15,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  return res.status(200).json({ success: true, data: [] })
-}
+  if(req.method === 'GET') {
+    const students = await prisma.student.findMany()
+    return res.status(200).json({ success: true, data: [...students] })
+
+}}
--- oppgave_2/pages/index.tsx
+++ oppgave_2/pages/index.tsx
@@ -5,6 +5,9 @@ const Home: NextPage = () => {
   useEffect(() => {
     const handler = async () => {
       try {
+        const response = await fetch('/api/students')
+        const data = await response.json()
+        console.log(data)
       } catch (error) {
         console.log(error)
       }

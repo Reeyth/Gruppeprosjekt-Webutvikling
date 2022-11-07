ec77e7243fb1e7e79ef778b483fd781ed022d204
UllernJ <Ulrik.johannesen@gmail.com>
2022-11-07 15:21:31 +0100

    started partly on the API.

---
+++ oppgave_3/pages/api/calender.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  if(req.method === 'GET') {
+    try {
+      const data = await prisma.$queryRaw<any>`
+        SELECT 
+        Employee.id as Employee_id, Employee.name as Employee_Name, Employee.rules as rules,
+        Week.name as week, Day.name as day, Lunch.name as lunch
+        FROM Employee
+        INNER JOIN Day ON Employee.id = Day.employeeId
+        INNER JOIN Week ON Day.weekId = Week.id
+        INNER JOIN Lunch ON Day.lunchId = lunch.id
+  `
+    console.log(data[0])
+    return res.status(200).json(data)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      async () => {
+        await prisma.$disconnect()
+      }
+    }
+  } else {
+    return res.status(400).json({ success: false, message: 'Bad request' })
+  }
+}
--- oppgave_3/pages/index.tsx
+++ oppgave_3/pages/index.tsx
@@ -1,9 +1,19 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
+  const dataFetch = async () => {
+    try {
+      const response = await fetch('/api/calender')
+      const data = await response.json()
+      console.log(data)
+    } catch(error) {
+      console.error(error)
+    }
+  }
   return (
     <main>
       <h1>Lunsjkalender</h1>
+      <button onClick={dataFetch}>Get data</button>
     </main>
   )
 }

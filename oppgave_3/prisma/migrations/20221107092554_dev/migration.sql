2e3d697dbf264468706a9a2d93712bedb55b478d
UllernJ <Ulrik.johannesen@gmail.com>
2022-11-07 12:20:49 +0100

    Updated the database.

--- oppgave_3/prisma/migrations/20221107092554_dev/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "employee" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "rules" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "employee_id_key" ON "employee"("id");
--- oppgave_3/prisma/migrations/20221107111524_dev/migration.sql
+++ oppgave_3/prisma/migrations/20221107111524_dev/migration.sql
@@ -1,14 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `employee` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "employee";
-PRAGMA foreign_keys=on;
-
 -- CreateTable
 CREATE TABLE "Employee" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -22,21 +11,22 @@ CREATE TABLE "Day" (
     "name" TEXT NOT NULL,
     "employeeId" INTEGER NOT NULL,
     "weekId" TEXT NOT NULL,
+    "lunchId" INTEGER NOT NULL,
     CONSTRAINT "Day_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "Employee" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "Day_weekId_fkey" FOREIGN KEY ("weekId") REFERENCES "Week" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "Day_weekId_fkey" FOREIGN KEY ("weekId") REFERENCES "Week" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Day_lunchId_fkey" FOREIGN KEY ("lunchId") REFERENCES "Lunch" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "Week" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "lunchId" TEXT NOT NULL,
-    CONSTRAINT "Week_lunchId_fkey" FOREIGN KEY ("lunchId") REFERENCES "Lunch" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    "name" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "Lunch" (
-    "id" TEXT NOT NULL PRIMARY KEY
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL
 );
 
 -- CreateIndex
@@ -49,4 +39,10 @@ CREATE UNIQUE INDEX "Day_id_key" ON "Day"("id");
 CREATE UNIQUE INDEX "Week_id_key" ON "Week"("id");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Week_name_key" ON "Week"("name");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Lunch_id_key" ON "Lunch"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Lunch_name_key" ON "Lunch"("name");

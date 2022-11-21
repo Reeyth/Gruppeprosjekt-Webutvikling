17e835bc7d93ad018c0a91e61868d027dd77c27d
Reeyth <78480777+Reeyth@users.noreply.github.com>
2022-11-21 15:21:08 +0100

    Component/File renaming

--- oppgave_3/components/Register.tsx
+++ oppgave_3/components/Register.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const WeekSpan = () => {
+const Register = () => {
   const [name, setName] = useState<String>('')
   const [rules, setRules] = useState<String[]>(['*'])
   const [ruleWeek, setRuleWeek] = useState<String>('')
@@ -132,4 +132,4 @@ const WeekSpan = () => {
   )
 }
 
-export default WeekSpan
+export default Register

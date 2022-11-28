3942f4d83ae5f36f9c9e2d72459ec7e3f71031f2
UllernJ <Ulrik.johannesen@gmail.com>
2022-11-28 13:04:25 +0100

    updated algo and added test

--- oppgave_3/__tests__/lunch.test.ts
+++ oppgave_3/__tests__/lunch.test.ts
@@ -1,6 +1,74 @@
-const algo = require('../prisma/algo');
+import { employees } from "../data/employees";
+import { feedMap, mapOfEmployees, createLunchList }  from "../prisma/algo";
+
+it("should return a list of employees", () => {
+  mapOfEmployees.set('all', [])
+  for(const employee of employees) {
+    feedMap(employee);
+  }
+  for(const key of mapOfEmployees.keys()) {
+    expect(mapOfEmployees.get(key).length).toBeGreaterThan(0);
+  }
+});
+
+const options = {
+  vacations: [8, 28, 29, 30, 31, 32, 40, 52],
+  yearSize: 52,
+  workDays: 5,
+  batchSize: 4,
+  maxOccurrences: 4,
+  days: [
+    'Mandag',
+    'Tirsdag',
+    'Onsdag',
+    'Torsdag',
+    'Fredag',
+    'Lørdag',
+    'Søndag',
+  ],
+}
+const data = createLunchList(options, mapOfEmployees);
 
 it('should be 52 weeks', () => {
-  let i = 1;
-    expect(i).toBe(1);
+  expect(data.length).toBe(52);
 })
+
+it("Should have 5 unique employees each week", () => {
+  for(const week of data) {
+    let employeesUsed = [];
+    for(const day of week) {
+      expect(employeesUsed.includes(day.employeeId)).toBe(false);
+      employeesUsed.push(day.employeeId);
+    }
+  }
+})
+
+it("There should be no employees working when vacation", () => {
+    for(const holidays of options.vacations) {
+      expect(data[holidays-1].length).toEqual(0);
+    }
+})
+describe('Validation', () => {
+  console.log('---Validation---');
+    it("Should fail when if employee apperas more than once in the same week", () => {
+      let employeesUsed = [1]
+        for(const day of data[0]) {
+          day.employeeId = 1;
+          expect(employeesUsed.includes(1)).toBe<boolean>(true);
+        }
+    })
+    it("Should fail when batch of size 4 have more than 1 occurance of the same employee", () => {
+      
+      for(let i = 0; i < options.yearSize / 4; i++) {
+        let employeesUsed = [];
+        for(let j = 0; j < options.batchSize; j++) {
+          for(const day of data[i * 4 + j]) {
+            expect(employeesUsed.includes(day.employeeId && day.name)).toBe<boolean>(false);
+            employeesUsed.push(day);
+          }
+        }
+      }
+    })
+    
+
+  })
--- oppgave_3/prisma/algo.ts
+++ oppgave_3/prisma/algo.ts
@@ -2,34 +2,36 @@ import { employees } from '../data/employees'
 import { faker } from '@faker-js/faker'
 import { PrismaClient } from '@prisma/client'
 
-const map = new Map()
-map.set('all', [])
+export const mapOfEmployees = new Map()
+mapOfEmployees.set('all', [])
 
-const feedMap = (employee: any) => {
+export const feedMap = (employee: any) => {
   //https://regexr.com/ is your friend
   const rulesDays = employee.rules.match(/(?!days:)([\d]+)|\*|even|odd/g)
-  if (rulesDays[1] !== undefined && map.has(rulesDays[1])) {
-    map.get(rulesDays[1]).push({ ...employee, count: 0, occourance: 0 })
+  if (rulesDays[1] !== undefined && mapOfEmployees.has(rulesDays[1])) {
+    mapOfEmployees.get(rulesDays[1]).push({ ...employee, count: 0, occourance: 0 })
     return
   } else if (rulesDays[1] !== undefined) {
-    map.set(rulesDays[1], [{ ...employee, count: 0, occourance: 0 }])
+    mapOfEmployees.set(rulesDays[1], [{ ...employee, count: 0, occourance: 0 }])
     return
   }
-  map.get('all').push({ ...employee, count: 0, occourance: 0 })
+  mapOfEmployees.get('all').push({ ...employee, count: 0, occourance: 0 })
 }
 
 for (const employee of employees) {
   feedMap(employee)
 }
-let regex = /(?!days:)([\d]+)|\*/g
-const workWeeks: any = []
-const createLunchList = (options: any) => {
+
+
+export const createLunchList = (options: any, map : Map<any, any>) => {
+  let regex = /(?!days:)([\d]+)|\*/g
+  const workWeeks: any = []
   let even: any = new Array(...map.get('all'), ...map.get('even'))
   let odd: any = new Array(...map.get('all'), ...map.get('odd'))
   const { vacations, yearSize, workDays, batchSize, maxOccurrences, days } =
     options
   let weekNumber = 0
-  for (let i = 0; i <= yearSize / batchSize; i++) {
+  for (let i = 0; i < yearSize / batchSize; i++) {
     for (const key of map.keys()) {
       for (const person of map.get(key)) {
         person.occourance = 0
@@ -50,6 +52,7 @@ const createLunchList = (options: any) => {
           employeeList = [...employeeList, ...map.get(key)]
         }
       }
+      let week : any = []
       for (let j = 0; j < workDays; j++) {
         if (vacations.includes(weekNumber)) {
           continue
@@ -68,17 +71,21 @@ const createLunchList = (options: any) => {
         employeeList[0].count++
         employeeList[0].occourance++
         employeesUsed.push(employeeList[0].name)
-        workWeeks.push({
+        week.push({
           name: days[j],
           employeeId: employeeList[0].id,
           weekId: weekNumber,
           lunchId: faker.datatype.number({ min: 1, max: 22 }),
         })
       }
+      workWeeks.push(week)
     }
   }
   return workWeeks
 }
+
+
+
 const currentOptions = {
   vacations: [8, 28, 29, 30, 31, 32, 40, 52],
   yearSize: 52,
@@ -95,20 +102,22 @@ const currentOptions = {
     'Søndag',
   ],
 }
-const randomizedLunchList = createLunchList(currentOptions)
+const randomizedLunchList = createLunchList(currentOptions, mapOfEmployees)
 
 const prisma = new PrismaClient()
 async function main() {
-  for (const lunchDay of randomizedLunchList) {
+  for (const week of randomizedLunchList) {
+    for(const day of week) {
     try {
       await prisma.day.create({
-        data: lunchDay,
+        data: day,
       })
     } catch (e) {
       console.log(e)
     } finally {
       await prisma.$disconnect()
     }
-  }
+  }}
 }
 main()
+

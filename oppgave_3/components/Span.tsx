588b01d78b2d6c8f9b6122330e718b1f8e9359bb
Peter Brännström <54285305+Brannstroom@users.noreply.github.com>
2022-11-30 02:33:57 +0100

    Changes to Span search and some styling
    The FROM and TO weeks is now interchangeable and will just swap the values if the FROM week is lower than TO.

--- oppgave_3/components/Span.tsx
+++ oppgave_3/components/Span.tsx
@@ -1,27 +1,46 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 const WeekSpan = () => {
-    
-    const [weekTarget1, setWeekTarget1] = useState<Number>(1)
-    const [weekTarget2, setWeekTarget2] = useState<Number>(1)
+  const [weekTarget1, setWeekTarget1] = useState<Number>(1)
+  const [weekTarget2, setWeekTarget2] = useState<Number>(1)
 
-    const handleWeek = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setWeekTarget1(Number(e.target.value))
-    }
-    const handleWeek2 = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setWeekTarget2(Number(e.target.value))
-    }
+  const handleWeek = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setWeekTarget1(Number(e.target.value))
+  }
+  const handleWeek2 = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setWeekTarget2(Number(e.target.value))
+  }
 
-    return (
-        <>
-        <h2>Hent frem kalenderen i en periode mellom to uker</h2>
-        <form action={`/span/${weekTarget1}/${weekTarget2}`}>
-        <input value={Number(weekTarget1)} onChange={handleWeek} min="1" max="52" type="number"></input>
-        <input value={Number(weekTarget2)} onChange={handleWeek2} min="1" max="52" type="number"></input>
-        <input type="submit" value="submit"/>
-        </form>
-        </>
-    )
+  return (
+    <div className="span-selection">
+      <h2>Hent frem kalenderen i en periode mellom to uker</h2>
+      <form action={`/span/${weekTarget1}/${weekTarget2}`}>
+        <label htmlFor="weekTarget1">Fra uke:</label>
+        <input
+          value={Number(weekTarget1)}
+          onChange={handleWeek}
+          min="1"
+          max="52"
+          type="number"
+        ></input>
+        
+        <br></br>
+        
+        <label htmlFor="weekTarget2">Til uke:</label>
+        <input
+          value={Number(weekTarget2)}
+          onChange={handleWeek2}
+          min="1"
+          max="52"
+          type="number"
+        ></input>
+
+        <br></br>
+
+        <input className="submit-span-selection" type="submit" value="Søk" />
+      </form>
+    </div>
+  )
 }
 
 export default WeekSpan
--- oppgave_3/pages/api/span/[...id].ts
+++ oppgave_3/pages/api/span/[...id].ts
@@ -14,6 +14,16 @@ export default async function handler(
         if(!weekId) {
             return res.status(400).json({ status: 400, message: 'Id missing' })
         }
+
+        /* Checking if the first value is larger than the second value,
+        if so, swap the values. This ensures that the first value is always
+        the smaller value. */
+        if(weekId[0] > weekId[1]) {
+            const temp = weekId[0]
+            weekId[0] = weekId[1]
+            weekId[1] = temp
+        }
+
         const data = await prisma.$queryRaw<any>`
         SELECT 
         Employee.name as employee_name, Day.name as day, Lunch.name as lunch_type, Day.id as id, Day.weekId as week_number

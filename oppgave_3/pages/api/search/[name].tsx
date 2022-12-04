107b3835157b9ff076d6e9b7e4b8318d6d01baa4
Peter Brännström <54285305+Brannstroom@users.noreply.github.com>
2022-12-04 19:10:06 +0100

    Response message for table

--- oppgave_3/pages/api/search/[name].tsx
+++ oppgave_3/pages/api/search/[name].tsx
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
@@ -7,13 +7,13 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Response>
 ) {
-    if(req.method === 'GET') {
-      try {
-        const name = req.query.name
-        if(!name) {
-            return res.status(400).json({ status: 400, message: 'User not found' })
-        }
-        const data = await prisma.$queryRaw<any>`
+  if (req.method === 'GET') {
+    try {
+      const name = req.query.name
+      if (!name) {
+        return res.status(200).json({ success: true, message: 'Navn på ansatt mangler' })
+      }
+      const data = await prisma.$queryRaw<any>`
         SELECT 
         Employee.name as employee_name, Day.name as day, Lunch.name as lunch_type, Day.id as id, Day.weekId as week_number
         FROM Employee
@@ -21,14 +21,20 @@ export default async function handler(
         INNER JOIN Lunch ON Day.lunchId = lunch.id
         WHERE Employee.name like ${name}
         ORDER BY Day.weekId ASC
-    `
-      return res.status(200).json(data)
-      } catch (error) {
-        console.error(error)
-      } finally {
-        async () => {
-          await prisma.$disconnect()
-        }
+      `
+
+      if(data.length == 0) {
+        return res.status(200).json({ success: true, data: data, message: 'Ingen ansatte med dette navnet' })
+      }
+      
+      return res.status(200).json({ success: true, data: data})
+    } catch (error) {
+      return res.status(400).json({ success: false, message: 'Bad request' })
+      console.error(error)
+    } finally {
+      ;async () => {
+        await prisma.$disconnect()
       }
     }
+  }
 }

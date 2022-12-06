78623df1677a0d3d3affc047b51c90e692dd817b
Reeyth <78480777+Reeyth@users.noreply.github.com>
2022-12-06 14:53:44 +0100

    More extensive testing of search

--- oppgave_3/cypress/e2e/search.cy.ts
+++ oppgave_3/cypress/e2e/search.cy.ts
@@ -1,5 +1,5 @@
-describe('Test of search', () => {
-  const testPerson = 'T'
+describe('Test of search with a specific search', () => {
+  const testPerson = 'Trude'
   it('Connects to page', () => {
     cy.visit('http://localhost:3000/search')
   })
@@ -10,7 +10,7 @@ describe('Test of search', () => {
   })
   it('Simulate request to API endpoint', () => {
     cy.request({
-      url : `localhost:3000/employee/${testPerson}`
+      url : `/api/search/${testPerson}`
     })
     .then((response) => {
       expect(response.status).to.be.oneOf([
@@ -23,7 +23,37 @@ describe('Test of search', () => {
     cy.get('.employeeName').should('contain', testPerson)
   })
   it('Excel file can be downloaded by clicking the button', () => {
-    cy.get('.excel-button').click()
+    cy.get('.excel-button').click({ multiple: true })
+    cy.readFile('../oppgave_3/cypress/downloads/Lunsjliste.xlsx')
+  })
+})
+
+describe('Test of search with a unspecific search', () => {
+  const testPerson = 'S'
+  it('Connects to page', () => {
+    cy.visit('http://localhost:3000/search')
+  })
+  it('Simulate search of an employee and successfully get a table of the employee', () => {
+    cy.get('#person').type(testPerson)
+    cy.get('.styled-button').click()
+    cy.url().should('include', testPerson)
+  })
+  it('Simulate request to API endpoint', () => {
+    cy.request({
+      url : `/api/search/${testPerson}`
+    })
+    .then((response) => {
+      expect(response.status).to.be.oneOf([
+        200,
+        304
+      ])
+    })
+  })
+  it('Should contain the correct data', () => {
+    cy.get('.employeeName').should('contain', testPerson)
+  })
+  it('Excel file can be downloaded by clicking the button', () => {
+    cy.get('.excel-button').click({ multiple: true })
     cy.readFile('../oppgave_3/cypress/downloads/Lunsjliste.xlsx')
   })
 })
